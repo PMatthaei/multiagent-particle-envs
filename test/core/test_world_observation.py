67fbceb95e8dc2a475f0c735629d6088171875a7
PMatthaei <pmd.matthaei@gmail.com>
2021-03-23 15:25:26 +0100

    Environment
    - New tests

---
+++ test/core/test_world_observation.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest.mock import Mock, MagicMock
+
+import numpy as np
+
+from multiagent.core import World
+from test.core.mock import mock_agent
+
+N_AGENTS = 2
+
+
+class WorldObservationTestCases(unittest.TestCase):
+    def setUp(self):
+        self.a = mock_agent(id=0)
+        self.b = mock_agent(id=1, tid=1)
+        self.b.sight_range = 3
+        self.a_spawn = np.array([0, 0])
+        self.b_spawn = np.array([1, 0])
+
+        self.world = World(grid_size=10, agents_n=N_AGENTS)
+        self.world.agents = [self.a, self.b]
+
+        self.world.connect(self.a, self.a_spawn)
+        self.world.connect(self.b, self.b_spawn)
+        self.world._update_visibility()
+
+        self.world._update_dist_matrix()
+
+        self.world._calculate_obs()
+
+    def test_a_observes_b(self):
+        #   vis     health    rel x-pos   rel y-pos  rel dist  unit bits
+        range = self.a.sight_range
+        a_obs_of_b = np.array([1.0, 1.0, 1.0 / range, 0.0 / range, 1.0 / range, 0, 0, 0])
+        np.testing.assert_array_equal(self.world.obs[self.a.id][self.b.id], a_obs_of_b)
+
+    def test_b_observes_a_with_different_sight_range(self):
+        range = self.b.sight_range
+        b_obs_of_a = np.array([1.0, 1.0, -1.0 / range, 0.0 / range, 1.0 / range, 0, 0, 0])
+        np.testing.assert_array_equal(self.world.obs[self.b.id][self.a.id], b_obs_of_a)
+
+
+if __name__ == '__main__':
+    unittest.main()
