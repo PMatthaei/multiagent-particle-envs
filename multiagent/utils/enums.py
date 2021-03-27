af78870f6baf9ce289294938f661e80e2add474c
PMatthaei <pmd.matthaei@gmail.com>
2021-03-27 10:10:36 +0100

    Environment
    - Enum en- and decoding

---
+++ multiagent/utils/enums.py
@@ -0,0 +1,30 @@
+import json
+
+from bin.team_plans_example import AI_SMALL
+from multiagent.core import RoleTypes, UnitAttackTypes
+
+PUBLIC_ENUMS = {
+    'RoleTypes': RoleTypes,
+    'UnitAttackTypes': UnitAttackTypes,
+}
+
+
+class EnumEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if type(obj) in PUBLIC_ENUMS.values():
+            return {"__enum__": str(obj)}
+        return json.JSONEncoder.default(self, obj)
+
+
+def as_enum(d):
+    if "__enum__" in d:
+        name, member = d["__enum__"].split(".")
+        return getattr(PUBLIC_ENUMS[name], member)
+    else:
+        return d
+
+
+if __name__ == '__main__':
+    t = json.dumps(AI_SMALL, cls=EnumEncoder)
+    t = json.loads(t, object_hook=as_enum)
+    assert t == AI_SMALL
