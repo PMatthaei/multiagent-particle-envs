5dfffd5a464b5310b59ba08f502581cb56d29e22
PMatthaei <pmd.matthaei@gmail.com>
2021-04-13 21:15:13 +0200

    Environment
    - Fix: Module reloading

--- multiagent/scenarios/team/__init__.py
+++ multiagent/scenarios/team/__init__.py
@@ -1,7 +1,12 @@
 import importlib.machinery
 import os.path as osp
+import sys
 
 
 def load(name):
     pathname = osp.join(osp.dirname(__file__), name)
-    return importlib.machinery.SourceFileLoader('', pathname).load_module()
+    if name not in sys.modules:
+        module_loaded = importlib.machinery.SourceFileLoader('', pathname).load_module()
+    else:
+        module_loaded = sys.modules.get(name)
+    return module_loaded
