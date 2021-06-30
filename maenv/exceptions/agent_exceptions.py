9990c103479cd071ccc055af71b8b7a4ebe0ec28
PMatthaei <pmd.matthaei@gmail.com>
2021-06-30 18:52:37 +0200

    Environment
    - Fixed wrong Exception Usage for illegal attack

--- maenv/exceptions/agent_exceptions.py
+++ maenv/exceptions/agent_exceptions.py
@@ -1,20 +1,17 @@
-from maenv.core import Agent
-
-
 class NoTargetFoundError(Exception):
     def __init__(self):
         super().__init__("Received attack action without target set. Check correctness")
 
 
 class IllegalTargetError(Exception):
-    def __init__(self, agent: Agent):
+    def __init__(self, agent):
         super().__init__(
             "The chosen target with id {0} can not be attacked/healed by agent with id {1}."
                 .format(agent.target_id, agent.id))
 
 
 class OverhealError(Exception):
-    def __init__(self, agent: Agent):
+    def __init__(self, agent):
         super().__init__(
             "The chosen target with id {0} can not be overhealed by agent with id {1}."
                 .format(agent.target_id, agent.id))
