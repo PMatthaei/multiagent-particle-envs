b452ba1919f0228e56436ae179186943ca2b9540
PMatthaei <pmd.matthaei@gmail.com>
2021-07-01 16:25:37 +0200

    Environment
    - Test if dead agents do not block moving onto their position

--- test/core/test_world_positions.py
+++ test/core/test_world_positions.py
@@ -22,11 +22,19 @@ class WorldPositionsTestCases(unittest.TestCase):
         self.world.connect(self.agent2, self.agent_spawn2)
 
     def test_update_pos_if_not_occupied(self):
-        self.agent.action.u[:2] = np.array([0, 1])  # move to -> 1,0 -> free
+        self.agent.action.u[:2] = np.array([0, 1])  # move to -> 1,2 -> free
         self.world._update_pos(self.agent)
 
         np.testing.assert_array_equal(self.world.positions[0], [1, 2])
         np.testing.assert_array_equal(self.agent.state.pos, [1, 2])
+
+    def test_update_pos_if_occupied_but_dead(self):
+        self.agent.action.u = np.array([0, -1])  # move to -> 1,0 -> occupied by dead agent 2
+        self.world.alive[1] = False # set dead
+        self.world._update_pos(self.agent)
+
+        np.testing.assert_array_equal(self.world.positions[0], [1, 0])
+        np.testing.assert_array_equal(self.agent.state.pos, [1, 0])
 
     def test_no_update_pos_if_occupied(self):
         self.agent.action.u = np.array([0, -1])  # move to -> 1,0 -> occupied by agent 2
